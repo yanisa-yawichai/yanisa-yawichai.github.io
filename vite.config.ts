717866a8831e58bc16b52ea5b1087dfff5154b33
yanisa-yawichai <yanisa.ywc@gmail.com>
2025-02-14 22:08:44 +0700

    fdfd

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/will-you-be-my-valentine",
   plugins: [react()],
+  base: '/yanisa-yawichai.github.io/', // 🔹 ตั้งให้ตรงกับชื่อ repo
+  build: {
+    outDir: 'dist',
+  },
 });
